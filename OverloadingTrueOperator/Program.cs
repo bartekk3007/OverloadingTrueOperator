39520fc17a366f6cd74bcbb2e496a5b75ab9ac21
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-02-28 23:44:32 +0100

    Add files via upload

---
+++ OverloadingTrueOperator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Vector v1 = new Vector(2, 3);
+        Console.WriteLine(v1);
+        if(v1)
+        {
+            Console.WriteLine($"{v1.X} <= {v1.Y}");
+        }
+        else
+        {
+            Console.WriteLine($"{v1.X} > {v1.Y}");
+        }
+
+        Vector v2 = new Vector(5, 2);
+        Console.WriteLine(v2);
+        if (v2)
+        {
+            Console.WriteLine($"{v2.X} <= {v2.Y}");
+        }
+        else
+        {
+            Console.WriteLine($"{v2.X} > {v2.Y}");
+        }
+    }
+}
---
+++ OverloadingTrueOperator/Vector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Vector
+{
+    public int X {  get; set; }
+    public int Y { get; set; }
+    public Vector(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public override string ToString()
+    {
+        return $"({X},{Y})";
+    }
+    public static bool operator true(Vector v)
+    {
+        if(v.X <= v.Y)
+        {
+            Console.WriteLine("True - true");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("True - false");
+            return false;
+        }
+    }
+    public static bool operator false(Vector v)
+    {
+        if(v.X > v.Y)
+        {
+            Console.WriteLine("False - true");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("False - false");
+            return false;
+        }
+    }
+}
